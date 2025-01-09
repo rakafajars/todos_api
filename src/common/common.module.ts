3c319e44da2b804680ddf62c40a25e6f49942402
Raka Fajar <rakafajar18@gmail.com>
2025-01-09 19:01:23 +0700

    Add Module Common

---
+++ src/common/common.module.ts
@@ -0,0 +1,18 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+  ],
+})
+export class CommonModule {}
